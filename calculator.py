71adbea3cd44b90d7ac12e8f29f60f6af7c67123
Hamza Shoaib <regedit1@hotmail.co.uk>
2011-11-18 21:42:22 +0000

    Progress with basic frame

---
+++ calculator.py
@@ -0,0 +1,24 @@
+import pygame
+import sys
+from pygame.locals import *
+
+pygame.init()
+
+bg = "background.jpg"
+screen = pygame.display.set_mode((400,400),0,32)
+background = pygame.image.load(bg).convert()
+pygame.display.set_caption("Calculator")
+
+
+
+#Game loop
+while 1:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+    screen.blit(background, (0,0))
+    pygame.display.update()
+    
