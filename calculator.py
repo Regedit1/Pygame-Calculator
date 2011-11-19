00d6851de1f87e642342cac4f2db340028608679
Hamza Shoaib <regedit1@hotmail.co.uk>
2011-11-19 12:50:57 +0000

    developed gui

--- calculator.py
+++ calculator.py
@@ -9,6 +9,7 @@ screen = pygame.display.set_mode((400,400),0,32)
 background = pygame.image.load(bg).convert()
 pygame.display.set_caption("Calculator")
 panel = pygame.image.load("panel.jpg").convert()
+numpad = pygame.image.load("numpad.jpg").convert()
 
 
 
@@ -18,10 +19,13 @@ while 1:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == MOUSEBUTTONDOWN:
+            x , y = pygame.mouse.get_pos()
 
 
     
     screen.blit(background, (0,0))
     background.blit(panel, (10,10))
+    background.blit(numpad, (20,100))
     pygame.display.update()
     
