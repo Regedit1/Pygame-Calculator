a5679a2964c96181076edfcc3103379269f821cc
Hamza Shoaib <regedit1@hotmail.co.uk>
2011-11-19 12:31:11 +0000

    added panel

--- calculator.py
+++ calculator.py
@@ -8,7 +8,7 @@ bg = "background.jpg"
 screen = pygame.display.set_mode((400,400),0,32)
 background = pygame.image.load(bg).convert()
 pygame.display.set_caption("Calculator")
-
+panel = pygame.image.load("panel.jpg").convert()
 
 
 
@@ -20,6 +20,8 @@ while 1:
             sys.exit()
 
 
+    
     screen.blit(background, (0,0))
+    background.blit(panel, (10,10))
     pygame.display.update()
     
