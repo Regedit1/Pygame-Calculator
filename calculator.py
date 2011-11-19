8f18378cd187c6b9c8d2be5d8f897d2695604f48
Hamza Shoaib <regedit1@hotmail.co.uk>
2011-11-19 14:51:36 +0000

    developed mouseinput

--- calculator.py
+++ calculator.py
@@ -13,16 +13,36 @@ numpad = pygame.image.load("numpad.jpg").convert()
 function = pygame.image.load("functions.jpg").convert()
 forn = "num"
 def funcornum(x,y):
-    if x > 20 and x < 220 and y > 100 and y < 300:
+    if x > 20 and x < 220 and y > 100 and y < 366:
         print "number"
         forn = "number"
     elif x > 300 and x < 350 and y > 100 and y < 300:
         print "function"
         forn = "function"
+    else:
+        forn = "neither"
     return forn
 
-def whichnumber():
-    print "testing for number"
+def whichnumber(x, y):
+    print "testing for number:"
+    if x > 20 and x < 86:
+        if y > 100 and y < 166:
+            print 1
+            number = 1
+        if y > 166 and y < 233:
+            print 4
+            number = 4
+        if y > 233 and y < 300:
+            print 7
+            number = 7
+        else:
+            number = "none"
+    else:
+        number =  "none"
+
+
+    return number
+
 
 #Game loop
 while 1:
@@ -34,7 +54,7 @@ while 1:
             x , y = pygame.mouse.get_pos()
             forn = funcornum(x , y)
             if forn == "number":
-                whichnumber()
+                whichnumber(x, y)
         
 
 
