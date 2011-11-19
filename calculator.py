22cff6d5e820a2ea3a66e9dbfe10cf74f706703b
Hamza Shoaib <regedit1@hotmail.co.uk>
2011-11-19 15:48:21 +0000

    Developed input

--- calculator.py
+++ calculator.py
@@ -11,6 +11,7 @@ pygame.display.set_caption("Calculator")
 panel = pygame.image.load("panel.jpg").convert()
 numpad = pygame.image.load("numpad.jpg").convert()
 function = pygame.image.load("functions.jpg").convert()
+equals = pygame.image.load("equals.jpg").convert()
 forn = "num"
 def funcornum(x,y):
     if x > 20 and x < 220 and y > 100 and y < 366:
@@ -55,6 +56,21 @@ def whichnumber(x, y):
             number = 0
         else:
             number = "none"
+    elif x > 152 and x < 220:
+        if y > 100 and y < 166:
+            print 3
+            number = 3
+        if y > 166 and y < 233:
+            print 6
+            number = 6
+        if y > 233 and y < 300:
+            print 9
+            number = 9
+        if y > 300 and y < 366:
+            print "."
+            number = "."
+        else:
+            number = "none"
         
     else:
         number =  "none"
@@ -82,5 +98,6 @@ while 1:
     background.blit(panel, (10,10))
     background.blit(numpad, (20,100))
     background.blit(function, (300,100))
+    background.blit(equals, (280,320))
     pygame.display.update()
     
